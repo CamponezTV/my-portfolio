8ec2ce69db8fbd5c8445582894c4ebbf2be299f6
Arthur Marinho <arthurcamponez2020@gmail.com>
2024-07-23 14:34:50 -0300

    version 0.1

---
+++ data/index.ts
@@ -0,0 +1,197 @@
+export const navItems = [
+    { name: "Sobre", link: "#about" },
+    { name: "Projetos", link: "#projects" },
+    { name: "Comentários", link: "#testimonials" },
+    { name: "Contato", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Constantemente desenvolvendo aplicativos baseados no que amo, sempre focando na criatividade e inovação",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-start",
+      img: "/b1.png",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Diversidade de línguas",
+      description: "Inglês, Português e Espanhol",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "Meu Stack de Tecnologias",
+      description: "Sempre evoluindo",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Amo contruir designs responsivos e desenvolve-los",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Construindo um Clone do Discord",
+      description: "Em desenvolvimento",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Quer entrar em contato?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Annapurna Website Tributo",
+      des: "Navegue pelos trailers e por alguns produtos dos jogos da Annapurna Interactive ",
+      img: "/p1.png",
+      iconLists: ["/js.png", "/html.png", "/css.png"],
+      link: "https://github.com/CamponezTV/Annapurna-Fanwebsite",
+    },
+    {
+      id: 2,
+      title: "Clone do Discord (Campfy)",
+      des: "Um Clone do discord feito com a minha cara e criatividade",
+      img: "",
+      iconLists: ["/next.svg", "/three.svg", "/ts.svg", "/tail.svg", "/c.svg"],
+      link: "",
+    },
+    {
+      id: 3,
+      title: "Portfólio Arthur Camponez",
+      des: "Um Portfólio feito para me destacar dentro do mercado super concorrido de TI.",
+      img: "/p3.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg", "/c.svg"],
+      link: "",
+    },
+    {
+      id: 4,
+      title: "Prototipo Figma",
+      des: "Uma prototipação de App Mobile anonimizado que produzi para um empresa focada na área de advocacia",
+      img: "/p4.png",
+      iconLists: ["/figma.png"],
+      link: "https://www.figma.com/design/m26tnhi6S0FzLjFPMzVuu6/Prototipe-Anonimizado-App-Advocacia?node-id=0-1&t=ld4ryrvMpWhc54vu-1",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "banestesdtvm",
+      nameImg: "/banestes.png",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      nameImg: "/sollitude.png",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Estagiário de Dev Software  2022",
+      desc: "Auxiliar a análise, desenvolvimento e implantação de soluções de software. Desenvolver e aplicar conhecimentos relacionados ao desenvolvimento de software, transformação digital e integração de sistemas.",
+      className: "md:col-span-2",
+      thumbnail: "/logo-b.png",
+    },
+    {
+      id: 2,
+      title: "Work Experience Program  2023",
+      desc: "Programa de intercâmbio cultural, desenvolvimento de outras línguas e experiência profissional em países diferentes",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/solitude.png",
+    },
+    {
+      id: 3,
+      title: "UX/UI Designer Freelancer 2024",
+      desc: "Freelancer na startup ISOS. Participo de reuniões semanais, realizo prototipação de aplicativos mobile no Figma além de possuir uma participação auxiliar no desenvolvimento front-end dos aplicativos",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/free.png",
+    },
+    {
+      id: 4,
+      title: "Estagiário de analista de TI 2024",
+      desc: "Desenvolvimento FullStack com ênfase em NextJS e Bootstrap. Resolução de problemas gerais de TI para otimização de processos. Participação ativa em reuniões estratégicas e decisões corporativas. Suporte eficaz às operações de comunicação e marketing.",
+      className: "md:col-span-2",
+      thumbnail: "/logodtvm.png",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
